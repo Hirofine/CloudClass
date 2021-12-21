ad536d8ee6ced600e5c799d1ff26e2440ee2f7a3
Hirofine <43345721+Hirofine@users.noreply.github.com>
2021-12-21 20:09:38 +0100

    things

--- cloud/lab4/user_interface/script.js
+++ cloud/lab4/user_interface/script.js
@@ -1,6 +1,41 @@
 
 $('.login').on('submit', function () {
-    var username = document.getElementById('username').value;
+    var username = document.getElementById('title').value;
     $('#print').append(username);
+    console.log(username);
     return false;
 });
+
+
+$('.output').on('submit', function () {
+   
+   //httpGetAsync('https://localhost:8000/books', )
+    //fetch('https://localhost:8000/books')
+    fetch('http://localhost:8000/books').then(function(response) {
+        return response.json();
+      }).then(function(data) {
+        console.log(data);
+      }).catch(function() {
+        console.log("Booo");
+      })
+    return false;
+});
+//get
+
+
+
+    //post
+    /*
+const params = {
+    param1: value1,
+    param2: value2
+};
+const options = {
+    method: 'POST',
+    body: JSON.stringify(params)
+};
+fetch('https://domain.com/path/', options)
+    .then(response => response.json())
+    .then(response => {
+        // Do something with response.
+    });*/
